d320c245b181df9deb31052196a8bd3b24c36398
waseem-akram-123 <waseemakram8660@gmail.com>
2024-10-06 15:31:04 +0530

    Add files via upload

---
+++ VARIABLES/Variables_and_Basic_Concepts.java
@@ -0,0 +1,32 @@
+public class Variables_and_Basic_Concepts {
+    public static void main(String[] args) {
+        
+        // Variable defination
+        // variables is the name for a memory location which stores some data
+
+        int age = 18;
+        int a = 5;
+        String name = "aman";
+
+        // rules
+        /* 1. variables are case sensitive 
+         * 2. 1st letter must be aplphabet or _ (underscore symbol)
+         * 3. no comma and blank spaces 
+         * 4. no symbol other tha underscore can be used
+        */
+
+        // keywords
+        /* keywords in java has special meaning which can't be used as an variable name */
+
+        // comments in java
+        // this is a comment
+
+        // IMPORTANT POINTS
+        /* 1. java is typed language
+         * 2. both intrepeted and compiled language
+         * 3. case sensitive language
+         * 4. written in CamelCase
+         */
+
+    }
+}
